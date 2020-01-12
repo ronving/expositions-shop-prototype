082a191857bb3c5b2d20f31b3a3c9bced27e3f15
ronving <getlight.m@gmail.com>
2020-01-13 00:37:04 +0300

    authorization

---
+++ src/main/java/com/ronving/controller/ContextListener.java
@@ -0,0 +1,42 @@
+package com.ronving.controller;
+
+import com.ronving.dao.UserDAO;
+import com.ronving.model.User;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.ronving.model.User.ROLE.ADMIN;
+import static com.ronving.model.User.ROLE.USER;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    /**
+     * Fake database connector.
+     */
+    private AtomicReference<UserDAO> dao;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+        dao = new AtomicReference<>(new UserDAO());
+
+        dao.get().add(new User(1, "Pavel", "1", ADMIN));
+        dao.get().add(new User(2, "Egor", "1", USER));
+
+        final ServletContext servletContext =
+                servletContextEvent.getServletContext();
+
+        servletContext.setAttribute("dao", dao);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        dao = null;
+    }
+
+
+}
---
+++ src/main/webapp/WEB-INF/view/admin_menu.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: getlight
+  Date: 09.01.2020
+  Time: 1:22
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>Menu</title>
+</head>
+<body>
+
+<h1>Hello ADMIN!</h1>
+<a href="<c:url value='/logout' />">Logout</a>
+</body>
+</html>
