905080ad0ba3b3168f26fc99b3d4ac1f15edb779
ronving <getlight.m@gmail.com>
2020-01-29 11:17:51 +0300

    session locale filter

---
+++ src/main/java/com/ronving/controller/filters/LocaleSessionFilter.java
@@ -0,0 +1,34 @@
+package com.ronving.controller.filters;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "LocaleSessionFilter", urlPatterns = {"/*"})
+public class LocaleSessionFilter implements Filter {
+    Logger LOGGER = Logger.getLogger(LocaleSessionFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+        if (req.getParameter("lang") != null) {
+            req.getSession().setAttribute("lang", req.getParameter("lang"));
+        }
+        filterChain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
